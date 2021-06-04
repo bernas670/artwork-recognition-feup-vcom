792c678062797306395eee455f99954b80213eaa
luispcunha <luispplmcunha@gmail.com>
2021-06-04 11:37:59 +0100

    cenas

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
+import os
 import pickle
 import numpy as np
 from sklearn.metrics import confusion_matrix
-from sklearn.model_selection import cross_validate, cross_val_predict
+from sklearn.model_selection import cross_validate, cross_val_predict, GridSearchCV
 from sklearn import svm
 
 from utils import plot_confusion_matrix
@@ -31,24 +32,36 @@ def train_classifier(classifier, X, y):
 
 
 # def grid_search():
+#     estimator = svm.SVC()
+
 #     param_grid = {
+#         'C': [0.1, 1, 10, 100],
+#         'kernel': ['rbf', 'poly', 'sigmoid'],
+#         'gamma': ['scale', 'auto'],
+#         'class_weight': [None, 'balanced'],
 #     }
+
 #     grid = GridSearchCV(
 #         estimator=estimator, param_grid=param_grid,
-#         cv=5, return_train_score=True,
+#         cv=5, return_train_score=True, refit=False,
 #         scoring=['accuracy', 'f1_macro', 'f1_micro'],
-#         n_jobs=-2,
+#         n_jobs=-2)
+
 #     results = grid.fit(X, y)
+#     print(results)
+
+#     with open(os.path.join('output', f'grid_results.p'), 'wb') as out_file:
+#         pickle.dump(results, out_file)
 
 
 if __name__ == '__main__':
     X, y = load_dataset("output/processed_1000.p")
     num_classes = len(set(y))
 
-    clf = svm.SVC()
+    clf = svm.SVC(kernel='poly')
 
     train_classifier(clf, X, y)
 
-    y_pred = cross_val_predict(clf, X, y)
+    y_pred = cross_val_predict(clf, X, y, cv=5, n_jobs=-2)
     conf_mx = confusion_matrix(y, y_pred)
     plot_confusion_matrix(conf_mx, num_classes)
